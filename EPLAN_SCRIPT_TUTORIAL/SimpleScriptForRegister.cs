4b13ddd7a38a774c802162ed34cf5572b0f2438d
bingyongcao <cby19941220@gmail.com>
2026-03-25 17:53:34 +0800

    Add project files.

---
+++ EPLAN_SCRIPT_TUTORIAL/SimpleScriptForRegister.cs
@@ -0,0 +1,66 @@
+﻿using Eplan.EplApi.Base;
+using Eplan.EplApi.Scripting;
+using System.Linq;
+using Eplan.EplApi.Gui;
+using System.Windows;
+
+namespace EPLAN_SCRIPT_TUTORIAL
+{
+    public class SimpleScriptForRegister
+    {
+        // set refreshAfterChanges true to avoid flickering problem.
+        RibbonBar myRibbonBar = new RibbonBar(true);
+
+        /// <summary>
+        /// The function with a 'DeclareAction' attribute can be registered as an action in EPLAN.
+        /// </summary>
+        [DeclareAction("MyScriptAction")]
+        public void MyScriptAction()
+        {
+            new Decider().Decide(
+                EnumDecisionType.eOkDecision,
+                "MyScriptAction was called!", 
+                "RegisterScriptAction", 
+                EnumDecisionReturn.eOK, 
+                EnumDecisionReturn.eOK);
+
+            return;
+        }
+
+        #region extend UI
+        [DeclareRegister]
+        public void RegisterRibbonItems()
+        {
+            CleanRibbonTab();
+
+            var newTab = myRibbonBar.AddTab(m_newTabName);
+            var cmdGroup = newTab.AddCommandGroup(m_commandGroupName, 0);
+            RibbonCommandInfo ribbonCommandInfo = new RibbonCommandInfo(m_commandName, "MyScriptAction")
+            {
+                Description = "a description of action",
+                IndexButtonPosition = 0,
+                Icon = new RibbonIcon(CommandIcon.Circle_0)
+            };
+            var command = cmdGroup.AddCommand(ribbonCommandInfo);
+        }
+
+        [DeclareUnregister]
+        public void UnRegisterRibbonItems()
+        {
+            CleanRibbonTab();
+        }
+
+        void CleanRibbonTab()
+        {
+            var newTab = myRibbonBar.Tabs
+                .FirstOrDefault(item => item.Name == m_newTabName);
+            if (newTab != null)
+                newTab.Remove();
+        }
+
+        public string m_newTabName = "EPLAN_SCRIPT_TUTORIAL";
+        public string m_commandGroupName = "Common";
+        public string m_commandName = "Button";
+        #endregion
+    }
+}
---
+++ EPLAN_SCRIPT_TUTORIAL/SimpleScriptWithParameters.cs
@@ -0,0 +1,31 @@
+﻿using Eplan.EplApi.Base;
+using Eplan.EplApi.Scripting;
+using System;
+
+namespace EPLAN_SCRIPT_TUTORIAL
+{
+    public class SimpleScriptWithParameters
+    {
+        /// <summary>
+        /// The function with a 'Start' attribute is the entry point of the script. 
+        /// It will be called by EPLAN when the script is executed.
+        /// Call script by command line: 
+        /// W3u.exe ExecuteScript /ScriptFile:"~\SimpleScriptWithParameters.cs" /Param1:"Hello" /Param2:"EPLAN"
+        /// </summary>
+        /// <param name="Param1"></param>
+        /// <param name="Param2"></param>
+        /// <returns></returns>
+        [Start]
+        public bool FunctionWithParameters(String Param1, String Param2)
+        {
+            new Decider().Decide(
+                EnumDecisionType.eOkDecision, 
+                Param1 + Param2, 
+                "SimpleScriptWithParams", 
+                EnumDecisionReturn.eOK, 
+                EnumDecisionReturn.eOK);
+
+            return true;
+        }
+    }
+}
