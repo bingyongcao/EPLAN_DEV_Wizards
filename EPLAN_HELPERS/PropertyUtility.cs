8c283b0ff9d8e1ff83872defc6886725d89a7049
bingyongcao <cby19941220@gmail.com>
2026-04-27 11:23:38 +0800

    add GetPropertyValueString to PropertyUtility

--- EPLAN_HELPERS/PropertyUtility.cs
+++ EPLAN_HELPERS/PropertyUtility.cs
@@ -1,5 +1,7 @@
+using Eplan.EplApi.Base;
 using Eplan.EplApi.DataModel;
 using System.Linq;
+using StorableObject = Eplan.EplApi.DataModel.StorableObject;
 
 namespace EplanHelpers
 {
@@ -14,6 +16,33 @@ namespace EplanHelpers
             if (propId == null) return null;
 
             return obj.Properties[propId];
+        }
+
+        public static string GetPropertyValueString(PropertyValue pValue)
+        {
+            switch (pValue.Definition.Type)
+            {
+                case PropertyDefinition.PropertyType.Bool:
+                    return pValue.ToBool() ? "YES" : "NO";
+                case PropertyDefinition.PropertyType.Long:
+                    return pValue.ToString();
+                case PropertyDefinition.PropertyType.Double:
+                    return pValue.ToDouble().ToString();
+                case PropertyDefinition.PropertyType.Coord:
+                    return $"({pValue.ToPointD().X}, {pValue.ToPointD().Y})";
+                case PropertyDefinition.PropertyType.String:
+                    return pValue.ToString();
+                case PropertyDefinition.PropertyType.Point:
+                    return $"({pValue.ToPointD().X}, {pValue.ToPointD().Y})";
+                case PropertyDefinition.PropertyType.Time:
+                    return pValue.ToTime().ToString();
+                case PropertyDefinition.PropertyType.MultilangString:
+                    return pValue.ToMultiLangString().GetStringToDisplay(ISOCode.Language.L_zh_CN);
+                case PropertyDefinition.PropertyType.ValueWithUnit:
+                    return pValue.ToString();
+                default:
+                    return null;
+            }
         }
     }
 }
